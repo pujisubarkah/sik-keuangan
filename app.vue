0af49eeecb31c97fc1492f0bff8d18b33bb6664d
unknown <keuanganlan@gmail.com>
2026-03-10 09:54:39 +0700

    fix: remove server-side @idds/vue imports to resolve Windows ESM loader error

--- app.vue
+++ app.vue
@@ -4,13 +4,3 @@
   </NuxtLayout>
 </template>
 
-<script setup>
-import { setBrandTheme } from '@idds/vue'
-
-// Set brand theme untuk LAN (Lembaga Administrasi Negara)
-setBrandTheme('lan')
-
-if (process.client) {
-  document.documentElement.setAttribute('data-theme', 'lan')
-}
-</script>
